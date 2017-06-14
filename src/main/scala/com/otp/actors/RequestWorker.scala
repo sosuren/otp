732ab3d39a187861bb6555b1a0c7593c2a10c6bd
Suren Yonjan <surendrayonjan@gmail.com>
2017-06-14 07:54:29 +0545

    request route to graph

--- src/main/scala/com/otp/actors/RequestWorker.scala
+++ src/main/scala/com/otp/actors/RequestWorker.scala
@@ -1,12 +1,26 @@
 package com.otp.actors
 
 import akka.actor.{ActorLogging, Actor}
+import org.opentripplanner.common.model.GenericLocation
+import org.opentripplanner.routing.core.RoutingRequest
+import org.opentripplanner.routing.impl.GraphPathFinder
 import org.opentripplanner.routing.services.GraphService
+
+import collection.JavaConverters._
 
 class RequestWorker (gs: GraphService) extends Actor with ActorLogging {
 
   def receive = {
     case x =>
       log.info(s"Worker received message: $x")
+      val routingReq = new RoutingRequest()
+      routingReq.from = new GenericLocation(45.49768, -122.75986)
+      routingReq.to = new GenericLocation(45.51790, -122.65686)
+      routingReq.setRoutingContext(gs.getRouter("pdx").graph)
+      val paths =  new GraphPathFinder(gs.getRouter("pdx")).getPaths(routingReq)
+      println(s"Paths length: ${paths.size()}")
+      paths.asScala.foreach { path =>
+        println(s"Walking distance can be: ${path.getWalkDistance}")
+      }
   }
 }
